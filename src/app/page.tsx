4298c990efde71ee51574a3a97e902130733f6e0
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2025-03-09 19:14:14 +0530

    feat: test web cam

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,18 @@
-export default function Home() {
-  <h1>Hello world</h1>
+/* eslint-disable @typescript-eslint/no-unused-vars */
+"use client"
+import React, { useRef } from 'react'
+import Webcam from 'react-webcam'
+
+type Props = object
+
+const HomePage = (props: Props) => {
+  const webcamRef = useRef<Webcam>(null);
+
+  return (
+    <div>
+      <Webcam />
+    </div>
+  )
 }
+
+export default HomePage
