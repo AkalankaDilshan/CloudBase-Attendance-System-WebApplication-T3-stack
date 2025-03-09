a47fe634ee2f927d4d10d07aee298ba1f8ca8faa
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2025-03-09 19:47:59 +0530

    chore: create layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 "use client"
+import { Separator } from '@/components/ui/separator'
 import React, { useRef, useState } from 'react'
 import Webcam from 'react-webcam'
 
@@ -12,8 +13,9 @@ const HomePage = (props: Props) => {
   const [mirrored, setMirrored] = useState<boolean>(false);
   return (
 
-    // left side
+
     <div className="flex h-screen">
+      {/* left side */}
       <div className="relative">
         <div className="relative h-screen w-full">
           <Webcam ref={webcamRef}
@@ -22,10 +24,33 @@ const HomePage = (props: Props) => {
           />
         </div>
       </div>
+
+      {/* Right side */}
+      <div className="flex flex-row flex-1">
+        <div className="border-primary/5 border-2 max-w-xs flex flex-col gap-2 justify-between  shadow-md rounded-md p-4">
+          {/* top section */}
+          <div className="flex flex-col gap-2">
+
+            <Separator />
+          </div>
+
+          {/* Middle section  */}
+          <div className="flex flex-col gap-2">
+            <Separator />
+
+            <Separator />
+          </div>
+
+
+          {/* Bottom section  */}
+          <div className="flex flex-col gap-2">
+
+            <Separator />
+          </div>
+        </div>
+
+      </div>
     </div>
-
-    //Right side
-
   )
 }
 
