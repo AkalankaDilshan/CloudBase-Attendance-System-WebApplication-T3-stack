31eaf829cec42ebc858f2dd565bc28de4e092a78
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2025-03-13 22:54:02 +0530

    fix: ctx error

--- utils/drow.ts
+++ utils/drow.ts
@@ -5,29 +5,29 @@ export function drawOnCanvas(
      predictions: DetectedObject[],
      ctx: CanvasRenderingContext2D | null | undefined
 ) {
-     if (!ctx) return; // Early return if ctx is null or undefined
+     if (ctx)  // Early return if ctx is null or undefined
 
-     predictions.forEach((detectedObject: DetectedObject) => {
-          const { class: name, bbox, score } = detectedObject;
-          const [x, y, width, height] = bbox;
+          predictions.forEach((detectedObject: DetectedObject) => {
+               const { class: name, bbox, score } = detectedObject;
+               const [x, y, width, height] = bbox;
 
-          if (score > 0.5) { // Assuming score is between 0 and 1
-               ctx.beginPath();
+               if (score > 0.5) { // Assuming score is between 0 and 1
+                    ctx.beginPath();
 
-               // Adjust x-coordinate if mirrored
-               const adjustedX = mirrored ? ctx.canvas.width - x - width : x;
+                    // Adjust x-coordinate if mirrored
+                    const adjustedX = mirrored ? ctx.canvas.width - x - width : x;
 
-               // Draw rectangle
-               ctx.fillStyle = name === 'person' ? '#FF0F0F' : '#00B612';
-               ctx.globalAlpha = 0.4;
-               ctx.rect(adjustedX, y, width, height);
-               ctx.fill();
+                    // Draw rectangle
+                    ctx.fillStyle = name === 'person' ? '#FF0F0F' : '#00B612';
+                    ctx.globalAlpha = 0.4;
+                    ctx.rect(adjustedX, y, width, height);
+                    ctx.fill();
 
-               // Draw text
-               ctx.font = "12px Courier New";
-               ctx.fillStyle = 'black';
-               ctx.globalAlpha = 1;
-               ctx.fillText(`${name} (${Math.round(score * 100)}%)`, adjustedX, y > 10 ? y - 5 : y + 10);
-          }
-     });
+                    // Draw text
+                    ctx.font = "12px Courier New";
+                    ctx.fillStyle = 'black';
+                    ctx.globalAlpha = 1;
+                    ctx.fillText(`${name} (${Math.round(score * 100)}%)`, adjustedX, y > 10 ? y - 5 : y + 10);
+               }
+          });
 }
