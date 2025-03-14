747328e7cb42e7280b4da20bb0e554d4048838c1
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2025-03-14 22:46:02 +0530

    chor: remove drawRoundedReact function

--- utils/drow.ts
+++ utils/drow.ts
@@ -37,12 +37,12 @@ export function drawOnCanvas(
 }
 
 // Fallback for rounded rectangle
-function drawRoundedRect(ctx: CanvasRenderingContext2D, x: number, y: number, width: number, height: number, radius: number) {
-     ctx.beginPath();
-     ctx.moveTo(x + radius, y);
-     ctx.arcTo(x + width, y, x + width, y + height, radius);
-     ctx.arcTo(x + width, y + height, x, y + height, radius);
-     ctx.arcTo(x, y + height, x, y, radius);
-     ctx.arcTo(x, y, x + width, y, radius);
-     ctx.closePath();
-}
+// function drawRoundedRect(ctx: CanvasRenderingContext2D, x: number, y: number, width: number, height: number, radius: number) {
+//      ctx.beginPath();
+//      ctx.moveTo(x + radius, y);
+//      ctx.arcTo(x + width, y, x + width, y + height, radius);
+//      ctx.arcTo(x + width, y + height, x, y + height, radius);
+//      ctx.arcTo(x, y + height, x, y, radius);
+//      ctx.arcTo(x, y, x + width, y, radius);
+//      ctx.closePath();
+// }
