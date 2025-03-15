6b3d0a7e36d711b0ae2190c10bc2102856e7a424
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2025-03-15 17:35:30 +0530

    feat: complete loading screen function

--- src/components/loading-screen/LoadingScreen.tsx
+++ src/components/loading-screen/LoadingScreen.tsx
@@ -1,7 +1,16 @@
+import CircleLoader from "react-spinners/CircleLoader";
 
-const LoadingScreen = () => {
+type LoadingScreenProps = {
+     loading: boolean;
+};
+
+const LoadingScreen = ({ loading }: LoadingScreenProps) => {
+
+     if (!loading) return null;
      return (
-          
+          <div className='z-50  px-4 absolute w-full h-full flex items-center justify-center bg-primary-foreground'>
+               Getting things read ...   <CircleLoader size={30} color='#ff0000' />
+          </div>
      );
 }
 
